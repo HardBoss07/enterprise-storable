ed0f3b7f0aa760cc19d549369afab74e3e9193dd
HardBoss07 <matteo@bosshard.net>
2026-01-23 15:54:44 +0100

    started work on first simple frontend

---
+++ web/components/Footer.tsx
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <footer>
+            <p>
+                Storable - All rights reserved
+                Created by <a href="https://m4tt3o.dev" target="_blank">Matteo Bosshard</a>
+            </p>
+        </footer>
+    );
+}
---
+++ web/components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header>
+            <a href="/">Storable</a>
+            <a href="/login">Login</a>
+        </header>
+    );
+}
