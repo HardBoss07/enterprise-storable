0befeadd5df767f3bfb3c5ee728f227e1c69df04
HardBoss07 <matteo@bosshard.net>
2026-03-08 20:42:10 +0100

    refactor: move user and filenode entities to common module

---
+++ storable-common/src/main/java/dev/m4tt3o/storable/common/entity/FileNode.java
@@ -0,0 +1,57 @@
+package dev.m4tt3o.storable.common.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "nodes")
+@Getter
+@Setter
+@NoArgsConstructor
+public class FileNode {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    private Long size;
+
+    private String mime;
+
+    @Column(name = "storage_key")
+    private String storageKey;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "modified_at")
+    private LocalDateTime modifiedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+
+    @Column(name = "owner_id", nullable = false)
+    private String ownerId;
+
+    @Column(name = "parent_id")
+    private Long parentId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private NodeKind kind;
+
+    public enum NodeKind {
+        file, folder
+    }
+}
