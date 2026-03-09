6f181539389a6eb71154bb9338b867a3e789dcce
HardBoss07 <matteo@bosshard.net>
2026-03-09 09:49:35 +0100

    created base user and file sturcture

--- db/schema.sql
+++ db/schema.sql
@@ -12,9 +12,6 @@ CREATE TABLE users (
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO users (id, username, email, password, role) VALUES 
-('00000000-0000-0000-0000-000000000000', 'guest', 'guest@storable.dev', '$2a$10$NotRealHashForGuestButNeedsToExist', 'GUEST');
-
 CREATE TABLE nodes (
     id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     owner_id VARCHAR(36) NOT NULL,
@@ -32,6 +29,15 @@ CREATE TABLE nodes (
     UNIQUE KEY node_unique_per_parent (parent_id, name),
     KEY node_parent_idx (parent_id)
 );
+
+INSERT INTO users (id, username, email, password, role)
+VALUES ('f43c0bcf-11e4-4629-b072-321ccd04e72a', 'root', 'root@m4tt3o.dev', '$2a$10$dA.gkjzVGZRgDOpwy63lfOIUtCpflbX8hKav4z55PiSNdILBPsHhq', 'ADMIN')
+ON DUPLICATE KEY UPDATE id=id;
+
+INSERT INTO nodes (id, owner_id, parent_id, name, kind, size, mime, storage_key)
+VALUES 
+(1, 'f43c0bcf-11e4-4629-b072-321ccd04e72a', NULL, 'root', 'folder', NULL, 'directory', '7a614f1d-53f8-44da-9785-1b58b449a8ba');
+
 
 CREATE USER 'backend_user'@'%' IDENTIFIED BY 'backend_password';
 GRANT SELECT, INSERT, UPDATE, DELETE ON storable.* TO 'backend_user'@'%';
--- db/test-data.sql
+++ db/test-data.sql
@@ -1,44 +1,36 @@
 USE storable;
 
--- 1. Create base user (Password is 'm4tt3o')
+-- 1. Create users
 INSERT INTO users (id, username, email, password, role)
-VALUES ('e17dbb63-16ab-484b-b6a4-59e0548eaa91', 'm4tt3o', 'matteobosshard@gmail.com', '$2a$10$J978U2UKWEl/sopXjTzl4Ox6zgggvxEkDt77FBehAF.CLEYqP8lC6', 'ADMIN')
+VALUES
+('e17dbb63-16ab-484b-b6a4-59e0548eaa91', 'm4tt3o', 'matteobosshard@gmail.com', '$2a$10$J978U2UKWEl/sopXjTzl4Ox6zgggvxEkDt77FBehAF.CLEYqP8lC6', 'USER'),
+('acd113ec-3452-4fa0-bddc-57d8bdfe2485', 'testuser', 'testuser@m4tt3o.dev', '$2a$10$tPXkkf.EH/XXsIo/ZthGRey4hBVjKkB4pfIZBnU7u1QoOiZogHX.W', 'USER')
 ON DUPLICATE KEY UPDATE id=id;
 
--- 2. Clean existing nodes
-DELETE FROM nodes;
-
--- 3. The Root
--- ID: 1
-INSERT INTO nodes (id, owner_id, parent_id, name, kind, size, mime, storage_key)
-VALUES (1, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', NULL, 'root', 'folder', NULL, 'directory', '7a614f1d-53f8-44da-9785-1b58b449a8ba');
-
--- 4. Level 1: Under Root
--- IDs: 2, 3
+-- 2. Home Directories (Parent is Root ID: 1)
 INSERT INTO nodes (id, owner_id, parent_id, name, kind, size, mime, storage_key)
 VALUES 
-(2, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', 1, 'guest', 'folder', NULL, 'directory', 'b3b83802-1579-48cc-821f-a55364eb495b'),
-(3, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', 1, 'm4tt3o', 'folder', NULL, 'directory', '6232b572-0107-436b-aef7-f4b2cfea5d00');
+(2, 'acd113ec-3452-4fa0-bddc-57d8bdfe2485', 1, 'testuser', 'folder', NULL, 'directory', 'b3b83802-1579-48cc-821f-a55364eb495b'),
+(3, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', 1, 'm4tt3o', 'folder', NULL, 'directory', '6232b572-0107-436b-aef7-f4b2cfea5d00')
+ON DUPLICATE KEY UPDATE id=id;
 
--- 5. Level 2: Under guest (ID 2) and m4tt3o (ID 3)
--- IDs: 4, 5, 6, 7
+-- 3. Content for testuser (Under ID 2)
 INSERT INTO nodes (id, owner_id, parent_id, name, kind, size, mime, storage_key)
 VALUES 
-(4, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', 2, 'documents', 'folder', NULL, 'directory', '8e23081f-24a6-483f-9306-0a11e83bf6c2'),
-(5, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', 2, 'random-guest-file.txt', 'file', 31, 'text/plain', '6c5106ff-54e6-4f8f-a159-85195763fc10'),
+(4, 'acd113ec-3452-4fa0-bddc-57d8bdfe2485', 2, 'documents', 'folder', NULL, 'directory', '8e23081f-24a6-483f-9306-0a11e83bf6c2'),
+(5, 'acd113ec-3452-4fa0-bddc-57d8bdfe2485', 2, 'random-testuser-file.txt', 'file', 31, 'text/plain', '6c5106ff-54e6-4f8f-a159-85195763fc10')
+ON DUPLICATE KEY UPDATE id=id;
+
+-- 4. Content for m4tt3o (Under ID 3)
+INSERT INTO nodes (id, owner_id, parent_id, name, kind, size, mime, storage_key)
+VALUES 
 (6, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', 3, 'files', 'folder', NULL, 'directory', 'a3354335-09cb-49c5-b564-65bdc69ead95'),
-(7, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', 3, 'test-file.txt', 'file', 36, 'text/plain', '8ae365c8-b2dc-44d8-968f-05f3fc1fe097');
+(7, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', 3, 'test-file.txt', 'file', 36, 'text/plain', '8ae365c8-b2dc-44d8-968f-05f3fc1fe097')
+ON DUPLICATE KEY UPDATE id=id;
 
--- 6. Level 3: Under guest/documents (ID 4) and m4tt3o/files (ID 6)
--- IDs: 8, 9, 10
+-- 5. Deeper levels...
 INSERT INTO nodes (id, owner_id, parent_id, name, kind, size, mime, storage_key)
 VALUES 
-(8, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', 4, 'projects', 'folder', NULL, 'directory', 'da50d160-3161-42ba-a6bf-b0326bc5c3d2'),
-(9, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', 4, 'document1.txt', 'file', 19, 'text/plain', 'a92fffd8-8286-4ac3-8c90-5488b105587f'),
-(10, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', 6, 'some-document.txt', 'file', 22, 'text/plain', '0c739db5-8341-4e5f-b639-01af13e12dc8');
-
--- 7. Level 4: Under guest/documents/projects (ID 8)
--- ID: 11
-INSERT INTO nodes (id, owner_id, parent_id, name, kind, size, mime, storage_key)
-VALUES 
-(11, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', 8, 'project-plan.txt', 'file', 26, 'text/plain', 'dffd5f1b-9b77-409a-a126-3be1e8e41351');
+(8, 'acd113ec-3452-4fa0-bddc-57d8bdfe2485', 4, 'projects', 'folder', NULL, 'directory', 'da50d160-3161-42ba-a6bf-b0326bc5c3d2'),
+(10, 'e17dbb63-16ab-484b-b6a4-59e0548eaa91', 6, 'some-document.txt', 'file', 22, 'text/plain', '0c739db5-8341-4e5f-b639-01af13e12dc8')
+ON DUPLICATE KEY UPDATE id=id;
