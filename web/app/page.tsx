fb6eb44e4bea4fb9fd6b9fe624f669a929b9cb68
HardBoss07 <matteo@bosshard.net>
2026-03-12 13:16:32 +0100

    moved file browser into features instead of directly in components

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,4 +1,4 @@
-import FileBrowser from "@/components/file-browser/FileBrowser";
+import FileBrowser from "@/components/features/file-browser/FileBrowser";
 
 export default function Home() {
   return <FileBrowser />;
