cc8e1b8f438e38d156732805450a5e40f3139268
HardBoss07 <matteo@bosshard.net>
2026-04-21 11:53:01 +0200

    chore: removed unused imports

--- storable-api/src/main/java/dev/m4tt3o/storable/api/security/JwtAuthenticationFilter.java
+++ storable-api/src/main/java/dev/m4tt3o/storable/api/security/JwtAuthenticationFilter.java
@@ -1,18 +1,8 @@
 package dev.m4tt3o.storable.api.security;
 
-import dev.m4tt3o.storable.core.security.JwtService;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.lang.NonNull;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
